6dffde548e584e9dc62e5e63ee07d6b17f95126c
Timothée Monin <timothee@monin.net>
2025-02-17 13:45:36 +0100

    Added affine cipher

--- classical_ciphers.py
+++ classical_ciphers.py
@@ -1,4 +1,5 @@
 from helper_functions import *
+from math import gcd
 
 
 def encrypt_caesar(text: str, shift: int, preserve_non_alphabetic_characters=False):
@@ -66,5 +67,53 @@ def decrypt_vigenere(text: str, key: str, preserve_non_alphabetic_characters=Fal
         ciphertext_letters.append(encrypted_letter)
 
         index += 1
+
+    return "".join(ciphertext_letters)
+
+
+def generate_affine_subsitution(a: int, b: int) -> dict:
+    """Generate the affine substitution defined by ax + b as a dictionary"""
+    if gcd(a, 26) != 1:
+        raise ValueError("Invalid value for a: a must be prime with 26")
+    mapping = {}
+    for letter in string.ascii_uppercase:
+        original_index = letter_index(letter) - 1  # Transforming 1-based indexing to 0-based indexing
+        transformed_index = (a * original_index + b) % 26
+        transformed_letter = letter_from_index(transformed_index + 1)  # Transforming 0-based to 1-based indexing
+        mapping[letter] = transformed_letter
+
+    return mapping
+
+
+def encrypt_affine(text: str, a: int, b: int, preserve_non_alphabetic_characters=False) -> str:
+    if not preserve_non_alphabetic_characters:
+        text = to_upper_case_without_punctuation_or_spaces(text)
+    substitution = generate_affine_subsitution(a, b)
+    ciphertext_letters = []
+    for letter in text:
+        if not letter.isalpha():
+            ciphertext_letters.append(letter)
+            continue
+
+        letter = letter.upper()
+        encrypted_letter = substitution[letter]
+        ciphertext_letters.append(encrypted_letter)
+
+    return "".join(ciphertext_letters)
+
+
+def decrypt_affine(text: str, a: int, b: int, preserve_non_alphabetic_characters=False) -> str:
+    if not preserve_non_alphabetic_characters:
+        text = to_upper_case_without_punctuation_or_spaces(text)
+    substitution = {v: k for k, v in generate_affine_subsitution(a, b).items()}
+    ciphertext_letters = []
+    for letter in text:
+        if not letter.isalpha():
+            ciphertext_letters.append(letter)
+            continue
+
+        letter = letter.upper()
+        encrypted_letter = substitution[letter]
+        ciphertext_letters.append(encrypted_letter)
 
     return "".join(ciphertext_letters)
