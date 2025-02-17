074b59404bb7bcccff75af8927c80d8fd4b11a9c
Timothée Monin <timothee@monin.net>
2025-02-17 16:41:50 +0100

    Added affine cipher interface

--- classical_ciphers_interface.py
+++ classical_ciphers_interface.py
@@ -2,12 +2,27 @@ from InquirerPy import inquirer
 from InquirerPy.validator import EmptyInputValidator
 from InquirerPy.base.control import Choice
 from InquirerPy.utils import color_print
-from classical_ciphers import encrypt_caesar, decrypt_caesar, encrypt_vigenere, decrypt_vigenere
+from classical_ciphers import (
+    encrypt_caesar,
+    decrypt_caesar,
+    encrypt_vigenere,
+    decrypt_vigenere,
+    encrypt_affine,
+    decrypt_affine,
+)
+from math import gcd
 
 
 def validate_number_between_1_and_26(num: str) -> bool:
     if num.strip().isnumeric():
         if int(num) >= 1 and int(num) <= 26:
+            return True
+    return False
+
+
+def validate_number_coprime_with_26(num: str) -> bool:
+    if num.strip().isnumeric():
+        if gcd(int(num), 26) == 1:
             return True
     return False
 
@@ -100,6 +115,60 @@ class VigenereInterface:
         return action_to_perform
 
 
+class AffineInterface:
+    EXIT_ACTION = "Exit affine cipher"
+    NEW_MESSAGE_ACTION = "Encrypt/decrypt new message"
+    CHANGE_KEY_ACTION = "Change coefficients"
+
+    def run(self):
+        self.ask_key()
+        self.encrypt_or_decrypt_message()
+        while True:
+            action = self.ask_action()
+            if action == self.EXIT_ACTION:
+                break
+            elif action == self.NEW_MESSAGE_ACTION:
+                self.encrypt_or_decrypt_message()
+            elif action == self.CHANGE_KEY_ACTION:
+                self.ask_key()
+
+    def ask_key(self):
+        a = inquirer.text(
+            message="Enter 'a' coefficient:",
+            instruction="(for ax + b)",
+            validate=validate_number_coprime_with_26,
+            invalid_message="a coefficient must be an integer coprime with 26",
+        ).execute()
+        self.a = int(a)
+
+        b = inquirer.text(
+            message="Enter 'b' coefficient:",
+            instruction="(for ax + b)",
+            validate=validate_number_between_1_and_26,
+            invalid_message="b coefficient must be an integer between 1 and 26",
+        ).execute()
+        self.b = int(b)
+
+    def encrypt_or_decrypt_message(self):
+        message = inquirer.text(message="Enter message to encrypt/decrypt:", multiline=True).execute()
+        encrypt_mode = inquirer.select(
+            message="Select action:",
+            choices=(Choice(True, name="Encrypt message"), Choice(False, name="Decrypt message")),
+        ).execute()
+        if encrypt_mode:
+            result = encrypt_affine(message, self.a, self.b)
+        else:
+            result = decrypt_affine(message, self.a, self.b)
+        color_print([("green", "Result:"), ("", " "), ("", result)])
+
+    def ask_action(self) -> str:
+        action_to_perform = inquirer.select(
+            message="Select next action:", choices=(self.NEW_MESSAGE_ACTION, self.CHANGE_KEY_ACTION, self.EXIT_ACTION)
+        ).execute()
+        return action_to_perform
+
+
 if __name__ == "__main__":
     CaesarInterface().run()
     VigenereInterface().run()
+    AffineInterface().run()
--- global_interface.py
+++ global_interface.py
@@ -1,5 +1,5 @@
 from enigma_interface import EnigmaInterface
-from classical_ciphers_interface import CaesarInterface, VigenereInterface
+from classical_ciphers_interface import CaesarInterface, VigenereInterface, AffineInterface
 from InquirerPy import inquirer
 from InquirerPy.utils import color_print
 from pyfiglet import Figlet
@@ -10,6 +10,7 @@ class GlobalInterface:
     ENIGMA_ACTION = "Enigma machine"
     CAESAR_ACTION = "Caesar cipher"
     VIGENERE_ACTION = "Vigenere cipher"
+    AFFINE_ACTION = "Affine cipher"
 
     def run(self):
         font = Figlet(font="slant")
@@ -24,10 +25,19 @@ class GlobalInterface:
                 CaesarInterface().run()
             elif action == self.VIGENERE_ACTION:
                 VigenereInterface.run()
+            elif action == self.AFFINE_ACTION:
+                AffineInterface().run()
 
     def ask_action(self):
         action_to_perform = inquirer.select(
-            "Select program:", choices=(self.ENIGMA_ACTION, self.CAESAR_ACTION, self.VIGENERE_ACTION, self.EXIT_ACTION)
+            "Select program:",
+            choices=(
+                self.ENIGMA_ACTION,
+                self.CAESAR_ACTION,
+                self.VIGENERE_ACTION,
+                self.AFFINE_ACTION,
+                self.EXIT_ACTION,
+            ),
         ).execute()
         return action_to_perform
 
